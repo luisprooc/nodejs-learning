0820fd62ac77064319d9156dbd449538963481be
Luis Rosario <luisprooc@gmail.com>
2021-05-01 19:38:17 -0300

    Routes from vanilla server added

--- servers/vanilla/response-html.js
+++ servers/vanilla/response-html.js
@@ -7,11 +7,24 @@ const html = fs.readFileSync('./index.html');
 
 http.createServer((request,response) => {
 
+    const { url } = request;
+
     response.writeHead(200,{
         'Content-Type': 'text/html'
     });
 
-    response.write(html);
+    if( url === '/') response.write(fs.readFileSync(html))
+
+    else if (url === '/about') response.write('./about.html');
+
+    else{
+        response.writeHead(404,{
+            'Content-Type': 'text/html'
+        });
+
+        response.write('PAGE NOT FOUND');
+    }
+
     response.end();
 })
 .listen(8080);
