499053dff2970d036094e6f18e27321e3fd0d576
Luis <luisprooc@gmail.com>
2021-05-18 08:28:03 -0700

    CRUD CLI init

---
+++ sequelize-project/index.js
@@ -0,0 +1,38 @@
+const { CRUD } = require("./helpers");
+const db = require("./models");
+
+
+if(  process.argv.length <=2){
+    process.exit(0);
+}
+
+const args = process.argv.slice(2);
+
+const command = args[0].split(":")[0].substring(2);
+const entity = args[0].split(":")[1];
+
+switch(command){
+    case CRUD.CREATE:
+        const data = {};
+        args.slice(1).forEach(arg => {
+            const tmp = arg.split("=");
+            data[tmp[0].substring(2)] = tmp[1];
+        });
+        
+        db[entity]
+            .create(data)
+            .then(() => console.log("CREATED!!!"))
+            .catch(console.log)
+
+        break;
+
+    case CRUD.READ:
+         db[entity]
+            .findAll()
+            .then(console.log)
+            .catch(console.log)
+        break;
+
+    default:
+        console.log("OPERATION NOT FOUND 💥");
+}
