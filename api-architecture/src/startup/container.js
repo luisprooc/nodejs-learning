161cc0e6a481e38ab0a6b4ebf44bb980b7b832cd
Luis Rosario <luisprooc@gmail.com>
2021-05-25 16:39:17 -0400

    ADD Structure finished

--- api-architecture/src/startup/container.js
+++ api-architecture/src/startup/container.js
@@ -1,15 +1,37 @@
 const {createContainer, asClass, asValue, asFunction} = require("awilix");
 
+// config
+const config = require("../config");
+
 // Services
 const {HomeService} = require("../services");
 
 // Controllers
 const {HomeController} = require("../controllers");
 
+//Routes
+const {HomeRoutes} = require("../routes/index-routes");
+const Routes = require("../routes");
+
+// APP
+const app = require(".")
+
 const container = createContainer();
 
-container.register({
-    HomeService: asClass(HomeService).singleton()
-});
+container
+    .register({
+        HomeService: asClass(HomeService).singleton()
+    })
+    .register({
+        HomeController: asClass(HomeController.bind(HomeController)).singleton()
+    })
+    .register({
+        HomeRoutes: asFunction(HomeRoutes).singleton()
+    })
+    .register({
+        router: asFunction(Routes).singleton(),
+        config: asValue(config),
+        app: asClass(app).singleton()
+    })
 
 module.exports = container;
